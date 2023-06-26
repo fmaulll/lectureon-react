78eb6252070e2d6e9585097ef4e91a04be3d826d
fmaulll <maul2821@gmail.com>
2023-06-26 13:50:18 +0700

    validate form

---
+++ src/hooks/useValidateForm.tsx
@@ -0,0 +1,23 @@
+import { UserTypes } from "../type";
+import useValidateEmail from "./useValidateEmail";
+
+export const useValidateForm = (data: UserTypes) => {
+    const error = []
+    if (Object.values(data).includes("")){
+        error.push("Input can't be empty!")
+    }
+
+    if (data.username.length < 4) {
+        error.push("Username must be 4 letters long!")
+    }
+
+    if (!useValidateEmail(data.email)) {
+        error.push("Email is not valid!")
+    }
+
+    if (data.password.length < 8) {
+        error.push("Password must contain 8 characters minimum!")
+    }
+
+    return error
+}
