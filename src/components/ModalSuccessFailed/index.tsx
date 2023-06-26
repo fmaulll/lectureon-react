c731ab7116fdde8e10fe1f338fa2755ec8a3f8a8
fmaulll <maul2821@gmail.com>
2023-06-26 13:49:14 +0700

    modal status succes or failed

---
+++ src/components/ModalSuccessFailed/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import OutsideWrapper from "../OutsideWrapper";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
+
+interface Props {
+  status: boolean;
+  message: string;
+  onClose: () => void;
+}
+
+const ModalSuccessFailed: FC<Props> = ({ status, message, onClose }) => {
+  return (
+    <div className="h-screen bg-[rgba(0,0,0,0.6)] w-full top-0 left-0 fixed flex justify-center items-center z-30">
+      <OutsideWrapper callback={onClose}>
+        <div className="bg-white rounded-xl flex justify-start items-center flex-col p-8 min-w-[500px] h-[450px]">
+          <h1 className="text-4xl font-bold">{status ? "Success" : "Failed"}</h1>
+          <p className="mt-8">{message}</p>
+          {status ? (
+            <FaCheckCircle className="mt-8" fill={"#32a852"} size={180} />
+          ) : (
+            <FaTimesCircle className="mt-8" fill={"red"} size={180} />
+          )}
+        </div>
+      </OutsideWrapper>
+    </div>
+  );
+};
+
+export default ModalSuccessFailed;
