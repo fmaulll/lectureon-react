49b1ed4c0dbc0e568a186fdfec10e956198655b2
fmaulll <maul2821@gmail.com>
2023-07-03 14:41:07 +0700

    added name to header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import ProfileMenu from "./ProfileMenu";
+import { LayoutContext } from "../../context/LayoutContext";
 
 const Header = () => {
+  const { user } = useContext(LayoutContext);
   const [open, setOpen] = useState<boolean>(false);
 
   return (
     <div className="h-[100px] px-8 flex items-center justify-between shadow-xl">
       <h1 className="font-bold text-2xl">Lecturon</h1>
-      <div className="flex justify-center items-center flex-col relative">
+      <div className="flex justify-center items-center relative">
+        <h3 className="font-bold mr-4">
+          {user?.firstName} {user?.lastName}
+        </h3>
         <FaUserCircle
           onClick={() => setOpen(!open)}
           className="cursor-pointer"
