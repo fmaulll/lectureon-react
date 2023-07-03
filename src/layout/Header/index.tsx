b1236a74647f51d853ced43edbc816e90aa54440
fmaulll <maul2821@gmail.com>
2023-07-03 09:17:17 +0700

    add user name in header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,10 +1,18 @@
-const Header = () => {
-  return (
-    <div className='h-[100px] px-8 flex items-center shadow-xl'>
-        <h1>Lecturon</h1>
-        
-    </div>
-  )
-}
+import { useContext } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { LayoutContext } from "../../context/LayoutContext";
 
-export default Header
+const Header = () => {
+  const { user } = useContext(LayoutContext);
+  return (
+    <div className="h-[100px] px-8 flex items-center justify-between shadow-xl">
+      <h1 className="font-bold text-2xl">Lecturon</h1>
+      <div className="flex justify-center items-center">
+        <FaUserCircle size={30} />
+        <h3>{user?.username}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
