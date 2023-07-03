7326287500ef182dd34c4d6fb8acbbb5fc7287d5
fmaulll <maul2821@gmail.com>
2023-07-03 10:44:01 +0700

    improve header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,15 +1,20 @@
-import { useContext } from "react";
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { LayoutContext } from "../../context/LayoutContext";
+import ProfileMenu from "./ProfileMenu";
 
 const Header = () => {
-  const { user } = useContext(LayoutContext);
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <div className="h-[100px] px-8 flex items-center justify-between shadow-xl">
       <h1 className="font-bold text-2xl">Lecturon</h1>
-      <div className="flex justify-center items-center">
-        <FaUserCircle size={30} />
-        <h3>{user?.username}</h3>
+      <div className="flex justify-center items-center flex-col relative">
+        <FaUserCircle
+          onClick={() => setOpen(!open)}
+          className="cursor-pointer"
+          size={40}
+        />
+        {open && <ProfileMenu onClose={() => setOpen(!open)} />}
       </div>
     </div>
   );
