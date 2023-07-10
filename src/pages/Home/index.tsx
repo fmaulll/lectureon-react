7ac7b060b833b1f8e2bd4756d03fd5861dc20b65
fmaulll <maul2821@gmail.com>
2023-07-10 15:27:37 +0700

    changes in home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState, useContext } from "react";
 import { LayoutContext } from "../../context/LayoutContext";
 import { PostTypes } from "../../type";
+import { url } from "../../helper";
 
 const Home = () => {
   const { setLoading, setMessage, setStatus } = useContext(LayoutContext);
@@ -11,18 +12,19 @@ const Home = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const result = await axios.get("http://localhost:9000/api/post");
+        const result = await axios.get(`${url}/post`);
 
         if (result.status === 200) {
-          setData(result.data);
+          const arr = result.data.map((item: any) => {
+            return {
+              ...item,
+              images: JSON.parse(item.images) || []
+            }
+          });
+          setData(arr);
         }
       } catch (error: any) {
-        setMessage(
-          "Log in failed! " +
-            String(
-              error.response.data.message ? error.response.data.message : error
-            )
-        );
+        setMessage("Failed to get content! " + String(error));
         setStatus(false);
       } finally {
         setLoading(false);
@@ -35,8 +37,10 @@ const Home = () => {
     <div>
       {data.map((post, index) => (
         <div key={index}>
-          <img className="w-[125px]" src={post.image} alt={post.subTitle} />
-          <iframe
+          {post?.images?.map((image, idx) => (
+            <img key={idx} className="w-[125px]" src={image} alt={post.subTitle} />
+          ))}
+          {/* <iframe
             width="560"
             height="315"
             src="https://www.youtube.com/embed/Y0Vo8x8ww7A"
@@ -44,7 +48,7 @@ const Home = () => {
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
-          ></iframe>
+          ></iframe> */}
           <h1>{post.title}</h1>
           <h2>{post.authorName}</h2>
           <h3>{post.subTitle}</h3>
