e812d32c63d2e686035eb63a9e6a5e78ffc73bdf
fmaulll <maul2821@gmail.com>
2023-07-15 00:15:17 +0700

    new post modal

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,7 +3,9 @@ import { LayoutContext } from "../context/LayoutContext";
 import ModalLoader from "../components/ModalLoader";
 import ModalSuccessFailed from "../components/ModalSuccessFailed";
 import Header from "./Header";
-
+import { useState } from "react";
+import NewPost from "../pages/NewPost";
+import {BsPlus} from "react-icons/bs"
 interface Props {
   children: React.ReactNode;
 }
@@ -11,15 +13,17 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => {
   const { loading, message, status, user, setMessage, setStatus } =
     useContext(LayoutContext);
+  const [openNewPost, setOpenNewPost] = useState<boolean>(false);
   return (
     <div className="bg-[#f0f0f0]">
       {user ? (
-        <div className="min-h-screen">
+        <div className="min-h-screen relative">
           <Header />
           {children}
+          <div onClick={() => setOpenNewPost(!openNewPost)} className="fixed right-0 bottom-0 p-2 rounded-full bg-white mr-4 mb-4 cursor-pointer shadow-xl"><BsPlus size={60} /></div>
         </div>
       ) : (
-        <div className=" bg-simple-wallpaper h-screen px-24 flex items-center">
+        <div className=" bg-simple-wallpaper min-h-screen px-24 flex items-center">
           {children}
         </div>
       )}
@@ -34,6 +38,7 @@ const Layout: FC<Props> = ({ children }) => {
           }}
         />
       ) : null}
+      {openNewPost ? <NewPost onClose={() => setOpenNewPost(false)} /> : null}
     </div>
   );
 };
