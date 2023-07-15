2d837e6187cbc607c50388a9c2724facaf2cc42d
fmaulll <maul2821@gmail.com>
2023-07-15 19:28:40 +0700

    add new post, images still not working

--- src/components/NewPost/index.tsx
+++ src/components/NewPost/index.tsx
@@ -5,6 +5,8 @@ import OutsideWrapper from "../OutsideWrapper";
 import { BsCardImage } from "react-icons/bs";
 import * as React from "react";
 import { LayoutContext } from "../../context/LayoutContext";
+import axios from "axios";
+import { url } from "../../helper";
 
 const initialNewPost = {
   authorId: null,
@@ -18,7 +20,7 @@ interface Props {
 }
 
 const NewPost: FC<Props> = ({ onClose }) => {
-  const { user } = useContext(LayoutContext)
+  const { user, setMessage, setStatus, setLoading } = useContext(LayoutContext);
   const [dataRequest, setDataRequest] = useState<NewPostTypes>(initialNewPost);
 
   const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,35 +42,69 @@ const NewPost: FC<Props> = ({ onClose }) => {
     setDataRequest((prev) => {
       return {
         ...prev,
-        [key]: value
-      }
-    })
-  }
+        [key]: value,
+      };
+    });
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    const formData = new FormData()
-    formData.append("authorId", String(dataRequest.authorId))
-    formData.append("title", String(dataRequest.title))
-    formData.append("subTitle", String(dataRequest.subTitle))
-    formData.append("description", String(dataRequest.description))
-    formData.append("images", String(dataRequest.images))
-    console.log(JSON.stringify(formData))
-  }
+    e.preventDefault();
+    setLoading(true);
+    
+    const fetchData = async() => {
+      try {
+        const result = await axios.postForm(`${url}/post`, {
+          id: dataRequest.authorId,
+          title: dataRequest.title,
+          subTitle: dataRequest.subTitle,
+          description: dataRequest.description,
+          images: dataRequest.images
+        })
+        if (result.status === 201) {
+          setMessage("Success create a new post!");
+          setStatus(true);
+        }
+      } catch(error: any) {
+        setMessage("Failed to get content! " + String(error));
+        setStatus(false);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData()
+  };
 
   useEffect(() => {
-    setDataRequest({...initialNewPost, authorId: user?.sub});
+    setDataRequest({ ...initialNewPost, authorId: user?.sub });
   }, []);
   return (
     <div className="h-screen bg-[rgba(0,0,0,0.6)] w-full top-0 left-0 fixed flex justify-center items-center z-30">
       <OutsideWrapper callback={onClose}>
         <div className="bg-white rounded-xl flex justify-start items-center flex-col p-8 w-[500px]">
-          <form className="flex flex-col w-full justify-between h-full" onSubmit={handleSubmit}>
+          <form
+            className="flex flex-col w-full justify-between h-full"
+            onSubmit={handleSubmit}
+          >
             <div className="flex flex-col w-full">
               <h1 className="font-semibold text-xl mb-4">Add New Post</h1>
-              <input className="border border-[#242424] rounded-lg px-4 py-2 mb-4" placeholder="Title" type="text" onChange={(e) => handleChange("title", e.target.value)} />
-              <input className="border border-[#242424] rounded-lg px-4 py-2 mb-4" placeholder="Sub Title" type="text" onChange={(e) => handleChange("subTitle", e.target.value)} />
-              <textarea className="border border-[#242424] rounded-lg px-4 py-2 mb-4" placeholder="Description" onChange={(e) => handleChange("description", e.target.value)}></textarea>
+              <input
+                className="border border-[#242424] rounded-lg px-4 py-2 mb-4"
+                placeholder="Title"
+                type="text"
+                onChange={(e) => handleChange("title", e.target.value)}
+              />
+              <input
+                className="border border-[#242424] rounded-lg px-4 py-2 mb-4"
+                placeholder="Sub Title"
+                type="text"
+                onChange={(e) => handleChange("subTitle", e.target.value)}
+              />
+              <textarea
+                className="border border-[#242424] rounded-lg px-4 py-2 mb-4"
+                placeholder="Description"
+                onChange={(e) => handleChange("description", e.target.value)}
+              ></textarea>
               <div className="grid grid-cols-3 gap-4 overflow-y-scroll mb-4">
                 {dataRequest.images.map((image, index) => (
                   <div
@@ -96,7 +132,9 @@ const NewPost: FC<Props> = ({ onClose }) => {
                 </div>
               </div>
             </div>
-            <button className="bg-[#242424] rounded-lg p-4 text-white font-bold hover:bg-[#303030] active:bg-[#101010] duration-200">Submit</button>
+            <button className="bg-[#242424] rounded-lg p-4 text-white font-bold hover:bg-[#303030] active:bg-[#101010] duration-200">
+              Submit
+            </button>
           </form>
         </div>
       </OutsideWrapper>
