29dcc3e5ed705170ae748ae3963bea0c2db6b939
fmaulll <maul2821@gmail.com>
2023-07-17 14:55:45 +0700

    increase the zindex

--- src/components/ModalSuccessFailed/index.tsx
+++ src/components/ModalSuccessFailed/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const ModalSuccessFailed: FC<Props> = ({ status, message, onClose }) => {
   return (
-    <div className="h-screen bg-[rgba(0,0,0,0.6)] w-full top-0 left-0 fixed flex justify-center items-center z-30">
+    <div className="h-screen bg-[rgba(0,0,0,0.6)] w-full top-0 left-0 fixed flex justify-center items-center z-50">
       <OutsideWrapper callback={onClose}>
         <div className="bg-white rounded-xl flex justify-start items-center flex-col p-8 min-w-[500px] h-[450px]">
           <h1 className="text-4xl font-bold">{status ? "Success" : "Failed"}</h1>
