db5dba1c2164c4afb3aac149c309b7c3e152a3ee
fmaulll <maul2821@gmail.com>
2023-07-18 13:45:10 +0700

    side bar

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -1,7 +1,27 @@
+import { AiFillHome } from "react-icons/ai";
+
+const menu = [
+  {
+    name: "home",
+    label: "Home",
+    icon: <AiFillHome size={20} />,
+  },
+];
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="bg-[#f0f0f0] w-[300px] shadow-2xl fixed h-screen mt-[100px]">
+      {menu.map((item, index) => (
+        <div className="flex items-center px-4 py-4 border">
+          <div className="mr-2">
 
-export default Sidebar
+          {item.icon}
+          </div>
+          <h3 className="text-lg">{item.label}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
