04aa0cfbb68b2c77879a9fdcfabdf9b1900fae1e
fmaulll <maul2821@gmail.com>
2023-07-19 22:33:17 +0700

    click handler and active sidebar

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -1,33 +1,46 @@
 import { AiOutlineHome } from "react-icons/ai";
 import { LuNewspaper } from "react-icons/lu";
 import { HiOutlineUsers } from "react-icons/Hi";
+import { useNavigate } from "react-router-dom";
 
 const menu = [
   {
     name: "home",
     label: "Home",
     icon: <AiOutlineHome size={20} />,
-    url: "/home"
+    path: "/home"
   },
   {
     name: "post",
     label: "Post",
     icon: <LuNewspaper size={20} />,
-    url: "/post"
+    path: "/post"
   },
   {
     name: "lecturer",
     label: "Lecturer",
     icon: <HiOutlineUsers size={20} />,
-    url: "/lecturer"
+    path: "/lecturer"
   },
 ];
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const isActive = (path: string) => {
+    if (path === window.location.pathname) {
+      return true
+    }
+    return false
+  }
+
+  const handleClickMenu = (path: string) => {
+    navigate(path)
+  }
+
   return (
     <div className="bg-[#f0f0f0] w-[300px] shadow-2xl fixed h-screen mt-[100px]">
       {menu.map((item, index) => (
-        <div key={index} className="flex items-center px-4 py-4 border">
+        <div key={index} onClick={() => handleClickMenu(item.path)} className={`${isActive(item.path) && "bg-gray-300"} flex items-center px-4 py-4 border cursor-pointer`}>
           <div className="mr-2">
 
           {item.icon}
