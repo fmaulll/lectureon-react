dc54cffaa68276e6057de854c10eab026f774993
fmaulll <maul2821@gmail.com>
2023-07-20 15:14:18 +0700

    using dynamic sidebar menu

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -1,28 +1,5 @@
-import { AiOutlineHome } from "react-icons/ai";
-import { LuNewspaper } from "react-icons/lu";
-import { HiOutlineUsers } from "react-icons/Hi";
 import { useNavigate } from "react-router-dom";
-
-const menu = [
-  {
-    name: "home",
-    label: "Home",
-    icon: <AiOutlineHome size={20} />,
-    path: "/home"
-  },
-  {
-    name: "post",
-    label: "Post",
-    icon: <LuNewspaper size={20} />,
-    path: "/post"
-  },
-  {
-    name: "lecturer",
-    label: "Lecturer",
-    icon: <HiOutlineUsers size={20} />,
-    path: "/lecturer"
-  },
-];
+import { AuthRoutes } from "../../router";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -39,7 +16,7 @@ const Sidebar = () => {
 
   return (
     <div className="bg-[#f0f0f0] w-[300px] shadow-2xl fixed h-screen mt-[100px]">
-      {menu.map((item, index) => (
+      {AuthRoutes.map((item, index) => (
         <div key={index} onClick={() => handleClickMenu(item.path)} className={`${isActive(item.path) && "bg-gray-300"} flex items-center px-4 py-4 border cursor-pointer`}>
           <div className="mr-2">
 
