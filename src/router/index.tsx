2458f8bb7b9077a5a104b6afc1990a1eb46067df
fmaulll <maul2821@gmail.com>
2023-07-20 15:14:21 +0700

    dynamic sidebar menu

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,9 @@ import Lecturer from "../pages/Lecturer"
 import Login from "../pages/Login"
 import Signup from "../pages/Signup"
 import Classes from "../pages/Classes"
+import { AiOutlineHome } from "react-icons/ai"
+import { LuNewspaper } from "react-icons/lu"
+import { HiOutlineUsers } from "react-icons/Hi"
 
 export const NonAuthRoutes = [
     {
@@ -18,14 +21,23 @@ export const NonAuthRoutes = [
 export const AuthRoutes = [
     {
         path: "/home",
+        name: "home",
+        label: "Home",
+        icon: <AiOutlineHome size={20} />,
         component: <Home />
     },
     {
         path: "/lecturer",
+        name: "lecturer",
+        label: "Lecturer",
+        icon: <LuNewspaper size={20} />,
         component: <Lecturer />
     },
     {
         path: "/classes",
+        name: "classes",
+        label: "Classes",
+        icon: <HiOutlineUsers size={20} />,
         component: <Classes />
     },
 ]
