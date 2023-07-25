3e3d57e3fe0d6199d79ba252caf7ec298c11b784
fmaulll <maul2821@gmail.com>
2023-07-25 21:13:53 +0700

    add more validation

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -4,27 +4,36 @@ import { AuthRoutes } from "../../router";
 const Sidebar = () => {
   const navigate = useNavigate();
   const isActive = (path: string) => {
-    if (path === window.location.pathname) {
-      return true
+    if (window.location.pathname.includes(path)) {
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const handleClickMenu = (path: string) => {
-    navigate(path)
-  }
+    localStorage.setItem("path", path)
+    navigate(path);
+  };
 
   return (
     <div className="bg-[#f0f0f0] w-[300px] shadow-2xl fixed h-screen mt-[100px]">
-      {AuthRoutes.map((item, index) => (
-        <div key={index} onClick={() => handleClickMenu(item.path)} className={`${isActive(item.path) && "bg-gray-300"} flex items-center px-4 py-4 border cursor-pointer`}>
-          <div className="mr-2">
-
-          {item.icon}
+      {AuthRoutes.map((item, index) => {
+        if (!item.sidebar) {
+          return
+        }
+        return (
+          <div
+            key={index}
+            onClick={() => handleClickMenu(item.path)}
+            className={`${
+              isActive(item.path) && "bg-gray-300"
+            } flex items-center px-4 py-4 border cursor-pointer`}
+          >
+            <div className="mr-2">{item.icon}</div>
+            <h3 className="text-lg">{item.label}</h3>
           </div>
-          <h3 className="text-lg">{item.label}</h3>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
